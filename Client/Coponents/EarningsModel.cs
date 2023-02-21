4d924c3255ef99d696d64cdf326cbf23aeb7411a
Calum <cramsel2319@gmail.com>
2023-02-21 17:58:03 -0600

    Checkpoint for first component and page additions. This covers the second video in the crash course and shows the ba. This goes over the second vid in the crash course and covers components, pages, and Blazor basics for utilizing C#

---
+++ Client/Coponents/EarningsModel.cs
@@ -0,0 +1,24 @@
+﻿using FinanceMentor.Shared;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinanceMentor.Client.Coponents
+{
+    public class EarningsModel
+    {
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        //This is a custom Enum type that is defined in the shared project
+        //Note: This is FinanceMentor.Shared, NOT shared folder under client!
+        [Required]
+        public EarningCategory Category { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+    }
+}
